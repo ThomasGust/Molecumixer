025d8c55e55da5c0b5e0a203edbb2db929d7787e
Thomas Gust <64709003+ThomasGust@users.noreply.github.com>
2024-02-13 13:44:19 -0800

    Commit On Save

--- src/orientations.py
+++ src/orientations.py
@@ -76,7 +76,7 @@ def permute_edges(graph, chunks, maximum_hamming_distance):
     
     edge_attributes = graph.edge_attr
     permuted_edge_attributes = shuffle_n_m_matrix(edge_attributes, orientation_vector)
-    print(permuted_edge_attr-edge_attributes)
+    print(permuted_edge_attributes-edge_attributes)
     graph.edge_attr = permuted_edge_attributes
 
     return graph, orientation_vector
