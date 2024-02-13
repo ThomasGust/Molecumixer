4dfd215302c4f10b063f2b6b4e06dfafd7758130
Thomas Gust <64709003+ThomasGust@users.noreply.github.com>
2024-02-13 10:44:19 -0800

    Commit On Save

--- src/orientations.py
+++ src/orientations.py
@@ -63,7 +63,7 @@ def permute_edges(graph, chunks, maximum_hamming_distance):
     orientation_vector = get_orientation_vector(chunks, maximum_hamming_distance)
     edge_index = torch.permute(graph.edge_index, (1,0))
     print(edge_index.shape)
-    top = edge_index[:, 0].expand_dims(1)
+    top = edge_index[:, 0][:, None]
     print(top.shape)
     permuted_top = shuffle_n_m_matrix(top, orientation_vector)
 
