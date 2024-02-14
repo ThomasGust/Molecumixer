a78a0b30c955b17ffa005dcc87f6549dacc1bb97
Thomas Gust <64709003+ThomasGust@users.noreply.github.com>
2024-02-14 12:42:46 -0800

    Commit On Save

--- src/orientations.py
+++ src/orientations.py
@@ -61,8 +61,6 @@ def permute_nodes(graph, chunks, maximum_hamming_distance):
 def permute_edges(graph, chunks, maximum_hamming_distance):
     # Permutes on side of the edge index and the whole edge attributes according to a permutation vector with a fixed hamming distance
     # ADD TORCH.TENSOR TO ALL NUMPY AREAS WHEN REFERENCING THEM TO GRAPH
-
-    new_graph = graph
     
     orientation_vector = get_orientation_vector(chunks, maximum_hamming_distance)
     print(orientation_vector)
