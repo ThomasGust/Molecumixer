5c672127704c4255123444ed01c0cc77624f6d37
Thomas Gust <64709003+ThomasGust@users.noreply.github.com>
2024-02-14 09:26:02 -0800

    Commit On Save

--- src/orientations.py
+++ src/orientations.py
@@ -77,6 +77,7 @@ def permute_edges(graph, chunks, maximum_hamming_distance):
     new_edge_index[:, 1] = edge_index[:, 1]
 
     #print(edge_index-new_edge_index)
+    print(edge_index-new_edge_index)
     new_graph.edge_index = new_edge_index
     print(new_graph.edge_index-graph.edge_index)
 
