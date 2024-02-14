00de342a6d71b9e5c08bdf16c51558c11d7cc169
Thomas Gust <64709003+ThomasGust@users.noreply.github.com>
2024-02-14 09:19:25 -0800

    Commit On Save

--- src/orientations.py
+++ src/orientations.py
@@ -73,7 +73,7 @@ def permute_edges(graph, chunks, maximum_hamming_distance):
     new_edge_index[:, 0] = torch.tensor(permuted_top[:, 0])
     new_edge_index[:, 1] = edge_index[:, 1]
 
-    #permuted_edge_index = torch.permute(new_edge_index, (1,0))
+    print(edge_index-new_edge_index)
     graph.edge_index = new_edge_index
 
     
