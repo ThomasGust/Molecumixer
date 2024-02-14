b92e09e62e48433adf3ea6684f29876b57c0ded5
Thomas Gust <64709003+ThomasGust@users.noreply.github.com>
2024-02-14 12:38:34 -0800

    Commit On Save

--- src/data.py
+++ src/data.py
@@ -212,5 +212,5 @@ if __name__ == "__main__":
 
     attributes = torch.permute(permuted_edges['edge_attr'], (1, 0))
     print(mol1.edge_attr.shape, attributes.shape)
-    subtracted = mol1.edge_attr - attributes
-    print(subtracted)
+    #subtracted = mol1.edge_attr - attributes
+    #print(subtracted)
