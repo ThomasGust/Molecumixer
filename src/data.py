9340ccfffcbc62b39b6cb3d9623729c42b462d0d
Thomas Gust <64709003+ThomasGust@users.noreply.github.com>
2024-02-14 12:49:28 -0800

    Commit On Save

--- src/data.py
+++ src/data.py
@@ -203,6 +203,6 @@ if __name__ == "__main__":
     batch = next(iter(data_loader))
     mol1 = batch[0]
 
-    permuted_nodes = permute_nodes(mol1, 5, 3)
-    permuted_edges = permute_edges(mol1, 5, 3)
+    permuted_nodes = permute_nodes(mol1, 10, 3)
+    permuted_edges = permute_edges(mol1, 10, 3)
     print(permuted_edges)
