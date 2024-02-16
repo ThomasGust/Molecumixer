55d642b00dd816748e6b2cacf9679935e27d24ab
Thomas Gust <64709003+ThomasGust@users.noreply.github.com>
2024-02-16 10:30:18 -0800

    Commit On Save

--- src/orientations.py
+++ src/orientations.py
@@ -80,4 +80,4 @@ def permute_edges(graph, chunks, maximum_hamming_distance):
     
     return x
 
-def batchwise_node_permutation(graph_batch, chunks, maximum_hamming_distance)
+def batch_node_permutation(graph_batch, chunks, maximum_hamming_distance)
