e345932724ffffb759072abb9838462d7d3e6c85
Thomas Gust <64709003+ThomasGust@users.noreply.github.com>
2024-02-16 16:15:58 -0800

    Commit On Save

--- src/data.py
+++ src/data.py
@@ -203,4 +203,4 @@ if __name__ == "__main__":
     BATCH_SIZE = 32
     batch = next(iter(data_loader))
     permuted_nodes = permute_nodes(batch, BATCH_SIZE*5, int(BATCH_SIZE*5/2))
-    print(permuted_nodes['x'].shape, batch.x.shape)
+    split = split_tensor(permuted_nodes["x"], BATCH_SIZE)
