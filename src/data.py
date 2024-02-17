5528715d2618548f69629e3b520e8638cc5cd989
Thomas Gust <64709003+ThomasGust@users.noreply.github.com>
2024-02-16 16:26:01 -0800

    Commit On Save

--- src/data.py
+++ src/data.py
@@ -203,5 +203,5 @@ if __name__ == "__main__":
     BATCH_SIZE = 32
     batch = next(iter(data_loader))
     permuted_nodes = permute_nodes(batch, BATCH_SIZE*5, int(BATCH_SIZE*5/2))
-    split = permute_each_nodes(batch, None, None)
+    split = permute_each_nodes(batch, 5, 2)
     
