5d8c4e186598f5db36d9394cade47c4b1e01bf07
Thomas Gust <64709003+ThomasGust@users.noreply.github.com>
2024-03-13 09:14:50 -0700

    edits

--- src/data.py
+++ src/data.py
@@ -27,7 +27,7 @@ from orientations import permute_edges, permute_nodes, get_orientation_vector, p
 
 def calculate_descriptors(mol):
     #o = list(Descriptors.CalcMolDescriptors(mol).values())
-    return list(Descriptors.CalcMolDescriptors(mol))
+    return Descriptors.CalcMolDescriptors(mol)
 
 def calculate_graph_descriptors(mol):
     balabanJ = GraphDescriptors.BalabanJ(mol)
