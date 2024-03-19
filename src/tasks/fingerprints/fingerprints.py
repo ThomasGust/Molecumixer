6c9b7f3958d51b843fd6f612b9724bbbf0a1ccc0
Thomas Gust <64709003+ThomasGust@users.noreply.github.com>
2024-03-19 09:17:59 -0700

    Add object oriented FingerprintPredictionModel

--- src/tasks/fingerprints/fingerprints.py
+++ src/tasks/fingerprints/fingerprints.py
@@ -1,6 +1,46 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
 
 class FingerprintGenerator:
     """This object handles the generation of molecular fingerprints"""
 
 class FingerprintPredictionModel:
-    """Given an encoder model, this module will predict molecular fingerprints from a latent vector"""
+    """This module will learn to predict molecular fingerprints from a latent vector"""
+
+    def __init__(self, encoder_dim, hidden_dim, fingerprint_dims, activation=F.relu):
+        self.encoder_dim = encoder_dim
+        self.hidden_dim = hidden_dim
+        self.fingerprint_dims = fingerprint_dims
+
+        self.ls = []
+        self.fos = []
+
+        for dim in self.fingerprint_dims:
+            self.ls.append(nn.Linear(self.encoder_dim, self.hidden_dim))
+            self.fos.append(nn.Linear(self.hidden_dim, dim))
+        
+        self.activation = activation
+
+    
+    def forward(self, x):
+        xs = []
+
+        for i, _ in enumerate(self.ls):
+            _x = self.ls[i](x)
+            _x = self.activation(_x)
+            _x = self.fos[i](_x)
+        
+        return xs
+
+    def compute_loss(self, logits, labels):
+        """
+        An equal weight BCE loss for any binary fingerprints this model might try to predict
+        """
+        losses = []
+
+        for i, dim in enumerate(self.fingerprint_dims):
+            l = F.binary_cross_entropy_with_logits(logits[i], labels[i])
+            l /= dim
+        
+        return sum(losses)
