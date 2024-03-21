121e35afe7ddd91b466885b4e4cb66e4c8f5474c
Thomas Gust <64709003+ThomasGust@users.noreply.github.com>
2024-03-21 14:51:12 -0700

    refactor FingerprintCalculator

--- src/data/data.py
+++ src/data/data.py
@@ -26,55 +26,7 @@ from config import NODE_SHUFFLE_DECODER_DIMENSION
 
 import numpy as np
 
-class FingerprintCalculator:
-    """
-    This object is similar to our DescriptorCalculator, it will compute molecular fingerprints given any molecule
-    """
-
-    def __init__(self, fps=["mfp2", "mfp3", "maccs", "rdkfp", "avfp"]):
-        self.fps = fps
-
-        self.fp_d = {"mfp2":lambda mol: self.calculate_mfp(mol, 2),
-                     "mfp3":lambda mol: self.calculate_mfp(mol, 3),
-                     "maccs":lambda mol: self.calculate_maccs(mol),
-                     "rdkfp":lambda mol: self.calculate_rdkfp(mol),
-                     "avfp":lambda mol: self.calculate_avfp(mol)}
-    @staticmethod
-    def fp2np(fp):
-        """Converts an rdkit fingerprint bit vector into a numpy array"""
-        return np.frombuffer(fp.ToBitString().encode(), 'u1') - ord('0') 
-
-    def calculate_mfp(self, mol, r):
-        mfp = AllChem.GetMorganFingerprintAsBitVect(mol, r)
-        return self.fp2np(mfp)
-    
-    def calculate_maccs(self, mol):
-        maccs = MACCSkeys.GenMACCSKeys(mol)
-        return self.fp2np(maccs)
-    
-    def calculate_rdkfp(self, mol):
-        rdkfp = RDKFingerprint(mol)
-        return self.fp2np(rdkfp)
-    
-    def calculate_avfp(self, mol):
-        avfp = avalon.GetAvalonFP(mol)
-        return self.fp2np(avfp)
-    
-    def calculate_fingerprints(self, smiles):
-        #TODO I should add support for both a mol and a pre-embedded RdKit Molecule
-        o_d = {}
-        mol = Chem.MolFromSmiles(smiles)
-        mol = Chem.AddHs(mol)
-
-        for fp_type in self.fps:
-            computer = self.fp_d[fp_type]
-            fp = computer(mol)
-
-            o_d[fp_type] = fp
-        
-        return o_d
-
-# TODO, NOW THAT THE ABOVE HAS BEEN REFACTORED, I NEED TO FIND A SOLUTION FOR THE CODE BELOW
+# TODO, NOW THAT THE CALCULATORS HAVE BEEN REFACTORED, I NEED TO FIND A SOLUTION FOR THE CODE BELOW
 # AS AN ARCHITECTURE CHOICE, IT MIGHT ACTUALLY BE BETTER TO COMPUTE ANY PRE-TRAINING TARGETS ON THE FLY AND NOT STORE THEM IN A DATALOADER
 # FOR NOW, ANY OF THE CODE BENEATH WILL BE BROKEN
 def compute_sample():
--- src/tasks/fingerprints/fingerprints.py
+++ src/tasks/fingerprints/fingerprints.py
@@ -1,10 +1,59 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import numpy as np
 
-class FingerprintGenerator:
-    """This object handles the generation of molecular fingerprints"""
+from rdkit import Chem
+from rdkit.Chem import AllChem, MACCSkeys, RDKFingerprint
+from rdkit.Avalon import pyAvalonTools as avalon
 
+class FingerprintCalculator:
+    """
+    This object is similar to our DescriptorCalculator, it will compute molecular fingerprints given any molecule
+    """
+
+    def __init__(self, fps=["mfp2", "mfp3", "maccs", "rdkfp", "avfp"]):
+        self.fps = fps
+
+        self.fp_d = {"mfp2":lambda mol: self.calculate_mfp(mol, 2),
+                     "mfp3":lambda mol: self.calculate_mfp(mol, 3),
+                     "maccs":lambda mol: self.calculate_maccs(mol),
+                     "rdkfp":lambda mol: self.calculate_rdkfp(mol),
+                     "avfp":lambda mol: self.calculate_avfp(mol)}
+    @staticmethod
+    def fp2np(fp):
+        """Converts an rdkit fingerprint bit vector into a numpy array"""
+        return np.frombuffer(fp.ToBitString().encode(), 'u1') - ord('0') 
+
+    def calculate_mfp(self, mol, r):
+        mfp = AllChem.GetMorganFingerprintAsBitVect(mol, r)
+        return self.fp2np(mfp)
+    
+    def calculate_maccs(self, mol):
+        maccs = MACCSkeys.GenMACCSKeys(mol)
+        return self.fp2np(maccs)
+    
+    def calculate_rdkfp(self, mol):
+        rdkfp = RDKFingerprint(mol)
+        return self.fp2np(rdkfp)
+    
+    def calculate_avfp(self, mol):
+        avfp = avalon.GetAvalonFP(mol)
+        return self.fp2np(avfp)
+    
+    def calculate_fingerprints(self, smiles):
+        #TODO I should add support for both a mol and a pre-embedded RdKit Molecule
+        o_d = {}
+        mol = Chem.MolFromSmiles(smiles)
+        mol = Chem.AddHs(mol)
+
+        for fp_type in self.fps:
+            computer = self.fp_d[fp_type]
+            fp = computer(mol)
+
+            o_d[fp_type] = fp
+        
+        return o_d
 class FingerprintPredictionModel:
     """This module will learn to predict molecular fingerprints from a latent vector"""
 
