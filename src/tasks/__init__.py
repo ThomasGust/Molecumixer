12323adeee9eabac5a4672141b1f3904e67963b1
Thomas Gust <64709003+ThomasGust@users.noreply.github.com>
2024-03-21 17:58:04 -0700

    update tasks\__init__.py

--- src/tasks/__init__.py
+++ src/tasks/__init__.py
@@ -1,1 +1,8 @@
 from tasks.common import Task
+
+from tasks.clusters import Clusterer, ClusterPredictionModel
+from tasks.descriptors import DescriptorCalculator, DescriptorPredictionModel
+from tasks.fingerprints import FingerprintCalculator, FingerprintPredictionModel
+from tasks.mask_fill import GraphMasker, GraphMaskPredictionModel
+from tasks.reconstruction import GVAE, GraphDiscriminator
+from tasks.shuffling import NodeShuffler, ShufflingModel
