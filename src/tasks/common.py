b8d2e38cc37270c119fe2f0c94262b76fcd08e87
Thomas Gust <64709003+ThomasGust@users.noreply.github.com>
2024-03-23 16:44:18 -0700

    Add task parent class

--- src/tasks/common.py
+++ src/tasks/common.py
@@ -1,3 +1,9 @@
 
 class Task:
     """This object will be the base class for all other pretraining tasks"""
+
+    def __init__(self, name):
+        self.name = name
+    
+    def task_step(self, batch):
+        pass
